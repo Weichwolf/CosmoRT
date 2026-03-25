6ceecf9836545fd861407452fe1444c1f4735909
Weichwolf <56591353+Weichwolf@users.noreply.github.com>
2026-03-25 16:41:09 +0100

    arch_shutdown: hlt-Loop statt cli (ACPI braucht Interrupts), kein Fehlermeldungs-Race
    532 PASS, 0 FAIL, 32s.

--- src/arch/x86_64/qemu.c
+++ src/arch/x86_64/qemu.c
@@ -8,10 +8,11 @@
  * QEMU Q35:    PM1a_CNT at 0x404
  * Bochs/OVMF:  PM1a_CNT at 0xB004 */
 void arch_shutdown(void) {
-    /* S5 sleep type = 0x2000 (SLP_TYP=5 << 10 | SLP_EN=1 << 13) */
+    /* S5 sleep type = 0x2000 (SLP_TYP=5 << 10 | SLP_EN=1 << 13).
+     * Try all known PM1a_CNT ports. QEMU exits on the right one.
+     * Loop prevents serial_puts from racing before QEMU terminates. */
     __asm__ volatile("outw %0, %1" :: "a"((unsigned short)0x2000), "Nd"((unsigned short)0x604));
     __asm__ volatile("outw %0, %1" :: "a"((unsigned short)0x2000), "Nd"((unsigned short)0x404));
     __asm__ volatile("outw %0, %1" :: "a"((unsigned short)0x2000), "Nd"((unsigned short)0xB004));
-    serial_puts("arch_shutdown: ACPI failed\n");
-    __asm__ volatile("cli; hlt");
+    for (;;) __asm__ volatile("hlt");
 }
